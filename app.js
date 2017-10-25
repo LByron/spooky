529367f62039c20930c0a60656212f7e2a2a96ac
Andrei <Andrei@DESKTOP-CG2I22O>
2017-10-25 17:02:31 +0300

    status report

--- app.js
+++ app.js
@@ -1,58 +1,67 @@
-var Spooky = require('spooky');
-process.env.PHANTOMJS_EXECUTABLE = 'node_modules/phantomjs/lib/phantom/bin/phantomjs';
+var Sitemapper = require('sitemapper');
+var _ = require('lodash');
+var Promise = require('bluebird');
+var axios = require('axios');
+const request = require('superagent');
 
 
 
-var url ='https://itunes.apple.com/us/app/facebook/id284882215?mt=8&v0=';
+var url ='https://www.lego.com/sitemaps/sitemapindex.xml';
 
+var sitemap = new Sitemapper();
 
-var spooky = new Spooky({
-    child: {
-        command: "node_modules/casperjs/bin/casperjs",
-        transport: 'http'
-    },
-    casper: {
-        logLevel: 'debug',
-        verbose: true
-    }
-}, function (err) {
-    if (err) {
-        e = new Error('Failed to initialize SpookyJS');
-        e.details = err;
-        throw e;
-    }
+sitemap.fetch(url).then(function (res) {
+    return getAllStatuses(res.sites.slice(0,300))
+})
+    .then(getReport).then(function (report) {
+        console.log(report);
+});
 
-    spooky.start(url);
+function getAllStatuses(sites){
+    return _.reduce(sites, function(result, url, key) {
+        result.push(getStatus(url))
+        return result;
+    }, []);
+}
 
-    spooky.then(function getHTML(response){
-        //this.emit('display','status:'+ JSON.stringify(response));
-        this.emit('display','html:'+ this.getHTML());
-
+function getReport(statuses) {
+    return Promise.all(statuses).then(function(response) {
+         var groupByStatus = _.groupBy(response, 'status');
+         return _.reduce(groupByStatus, function(result, value, key) {
+            result[key] = {
+                count : value.length
+            };
+            return result;
+        }, {});
     });
+}
 
-    spooky.run();
+function getStatus(url) {
+    return request.get(url)
+        .timeout({
+            response: 5000,  // Wait 5 seconds for the server to start sending,
+            deadline: 60000, // but allow 1 minute for the file to finish loading.
+        })
+        .then(function (res) {
+            return {
+                url : url,
+                status : res.status
+            }
+        })
+        .catch(function (error) {
+            if(error.response) {
+                return {
+                    url: url,
+                    status: error.response.status
+                }
+            } else {
+                console.log('xxx', url);
+                return {
+                    url : url,
+                    status : 'UNKNOWN'
+                }
+            }
+        });
 
 
-});
-
-
-
-
-spooky.on('error', function (e, stack) {
-    console.error(e);
-
-    if (stack) {
-        console.log(stack);
-    }
-});
-
-
-spooky.on('display', function (text) {
-    console.log(text);
-});
-
-spooky.on('log', function (log) {
-    if (log.space === 'remote') {
-        console.log(log.message.replace(/ \- .*/, ''));
-    }
-});
+}
